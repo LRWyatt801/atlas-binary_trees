4d1a6347bc29ea069ee2a4feea530f83151c31c9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-04-16 13:55:59 -0500

    Fixed for height of subnodes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - measures the balance factor
@@ -11,14 +12,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int height_l = 0, height_r = 0;
 
-	if (!tree)
-		return (0);
-
-	if (tree->left)
-		height_l = 1 + binary_tree_balance(tree->left);
-
-	if (tree->right)
-		height_r = 1 + binary_tree_balance(tree->right);
+	height_l = binary_tree_height(tree->left);
+	height_r = binary_tree_height(tree->right);
 	
 	return (height_l - height_r);
 }
