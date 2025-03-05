e872db4a9386da773021799f45f9cecf24eecbb2
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-05 15:29:54 -0600

    Add file and update header

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_avl - checks if a binary tree is an AVL tree
+* @tree: root of tree
+*
+* Return: int - 1 if true, otherwise 0
+*/
+
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	return (1);
+}
+
