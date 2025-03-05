c86083d2c526cee6f5aad4c27d3a377e9cef95da
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-05 16:05:25 -0600

    fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,10 +17,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	if (binary_tree_is_full(tree->left))
-	{
-		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-			return (1);
-	}
+	if (!binary_tree_is_full(tree->left))
+		return (0);
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+		return (0);
 	return (binary_tree_is_perfect(tree->left) * binary_tree_is_perfect(tree->right));
 }
